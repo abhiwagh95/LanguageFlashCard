70ef0de40a6ec82d1eebcfec866d9fac9a24c107
abhishekwagh95 <abhiw963@gmail.com>
2022-07-17 17:40:00 +0900

    added home UI for translate

--- app/src/main/java/com/example/languageflashcard/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/languageflashcard/ui/home/HomeFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.languageflashcard.databinding.FragmentHomeBinding
@@ -27,12 +30,26 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
+        val textView: TextView = binding.textviewMessage
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        search()
         return root
+    }
+
+    private fun search() {
+        val inputSearch: EditText = binding.inputText
+        val buttonSearch: Button = binding.buttonSearch
+        buttonSearch.text = "SEARCH"
+            buttonSearch.setOnClickListener {
+                Toast.makeText(
+                    context,
+                    "Result will be displayed shortly : " + inputSearch.text,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/languageflashcard/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/languageflashcard/ui/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.languageflashcard.ui.home
 
+import android.widget.EditText
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,7 +8,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Please Enter Word To Translate:"
     }
     val text: LiveData<String> = _text
 }
