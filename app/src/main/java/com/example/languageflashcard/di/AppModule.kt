cab670a97890bcc5982a78024a5357dc4578205b
UKMIITB <ujjwalkr.maharana@gmail.com>
2022-08-07 02:33:17 +0900

    init setup for dependency injection

---
+++ app/src/main/java/com/example/languageflashcard/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.example.languageflashcard.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    fun provideFirebaseFirestore(): FirebaseFirestore {
+        return Firebase.firestore
+    }
+}
