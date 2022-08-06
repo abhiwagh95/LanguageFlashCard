0e266e8b68cb1e606ab5992dc6e702bd99828629
UKMIITB <ujjwalkr.maharana@gmail.com>
2022-08-07 02:38:15 +0900

    configured firestore settings

--- app/src/main/java/com/example/languageflashcard/di/AppModule.kt
+++ app/src/main/java/com/example/languageflashcard/di/AppModule.kt
@@ -1,19 +1,29 @@
 package com.example.languageflashcard.di
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
+    @Singleton
     fun provideFirebaseFirestore(): FirebaseFirestore {
-        return Firebase.firestore
+        val firebaseFirestore = Firebase.firestore
+        val settings = FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(true)
+            .setCacheSizeBytes(FirebaseFirestoreSettings.CACHE_SIZE_UNLIMITED)
+            .build()
+
+        firebaseFirestore.firestoreSettings = settings
+        return firebaseFirestore
     }
 }
