0355c2416207f836f1e5dce3ed665044981504aa
abhishekwagh95 <abhiw963@gmail.com>
2022-08-07 16:17:58 +0900

    added flow to fetch google api response

--- app/src/main/java/com/example/languageflashcard/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/languageflashcard/ui/home/HomeViewModel.kt
@@ -1,12 +1,20 @@
 package com.example.languageflashcard.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.languageflashcard.model.GoogleAPIResponse
+import com.example.languageflashcard.model.Response
 import com.example.languageflashcard.model.Translate
 import com.example.languageflashcard.repository.FirebaseRepository
 import com.example.languageflashcard.repository.GoogleAPIRepository
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -27,9 +35,17 @@ class HomeViewModel @Inject constructor(
         query: String,
         target: String = "ja",
         source: String = "en"
-    ) = withContext(Dispatchers.IO) {
-        return@withContext googleAPIRepository.getGoogleAPIResponse(
-            query = query, target = target, source = source
-        )
-    }
+    ) = flow<Response<GoogleAPIResponse>> {
+        emit(Response.Loading())
+
+        val googleAPIResponseDeferred = viewModelScope.async {
+            googleAPIRepository.getGoogleAPIResponse(
+                query = query, target = target, source = source
+            )
+        }
+        emit(Response.Success(googleAPIResponseDeferred.await()))
+    }.catch {
+        emit(Response.Error(it.message.toString()))
+    }.flowOn(Dispatchers.IO)
+
 }
